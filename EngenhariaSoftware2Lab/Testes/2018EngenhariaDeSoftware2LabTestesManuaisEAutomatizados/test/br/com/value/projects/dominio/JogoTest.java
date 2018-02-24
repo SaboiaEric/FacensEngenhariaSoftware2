fad1b9290f06efd63e9aa2185e1c836b6287b834
Eric Saboia <saboia.eric@gmail.com>
2018-02-24 13:53:40 -0300

    Added project and included .java files and the first test

---
+++ EngenhariaSoftware2Lab/Testes/2018EngenhariaDeSoftware2LabTestesManuaisEAutomatizados/test/br/com/value/projects/dominio/JogoTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.value.projects.dominio;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Saboia
+ */
+public class JogoTest {
+    
+    public JogoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of anota method, of class Jogo.
+     */
+    @Test
+    public void testAnota() {
+        System.out.println("anota");
+        Resultado resultado = null;
+        Jogo instance = null;
+        instance.anota(resultado);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDescricao method, of class Jogo.
+     */
+    @Test
+    public void testGetDescricao() {
+        System.out.println("getDescricao");
+        Jogo instance = null;
+        String expResult = "";
+        String result = instance.getDescricao();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getResultados method, of class Jogo.
+     */
+    @Test
+    public void testGetResultados() {
+        System.out.println("getResultados");
+        Jogo instance = null;
+        List<Resultado> expResult = null;
+        List<Resultado> result = instance.getResultados();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
