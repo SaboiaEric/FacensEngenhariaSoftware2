d53382a2f28155ed10324c7892fc31a9768dd016
LAUREN MARIA FERREIRA <150017@LABINFO.FACENS.BR>
2018-03-20 22:29:21 -0300

    Iniciada a atividade 3 - criacao de requisitos e regra de negocio. By: Eric Saboia

---
+++ EngenhariaSoftware2Lab/Atividade3-Relatorio3/AtividadeM1Final/src/RequisitosRegras/EricRegrasRequisitos.java
@@ -0,0 +1,16 @@
+package RequisitosRegras;
+public class EricRegrasRequisitos {
+/* 
+    Requisitos Funcionais - Eric Saboia
+    RELEASE (Criacao de Jogos): Criar um jogo com as informacoes corretas
+    Descrição                                                                     C P E
+    RF1 **********************************************************************    I 1 8
+    RF2 **********************************************************************    D 1 3 
+    
+    
+    Regra de Negócio - Eric Saboia
+    Regra 1:
+    Regra 2:
+    
+*/    
+}
