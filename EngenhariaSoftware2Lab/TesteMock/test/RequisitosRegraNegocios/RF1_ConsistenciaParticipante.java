ad1dc3b752292db9f7517e85762601b5ad5614ec
Eric Saboia <saboia.eric@gmail.com>
2018-04-01 14:57:26 -0300

    Adicionado primeiro requisito RF1

---
+++ EngenhariaSoftware2Lab/TesteMock/test/RequisitosRegraNegocios/RF1_ConsistenciaParticipante.java
@@ -0,0 +1,13 @@
+package RequisitosRegraNegocios;
+
+/**
+ *
+ * @author Saboia
+ */
+public class RF1_ConsistenciaParticipante {
+    /*
+    Eric Saboia
+    Requisito: Consistência do participante
+    Regra de Negócio: o participante deve conter um nome, sendo ele diferente de vazio e com mais de uma letra
+    */
+}
